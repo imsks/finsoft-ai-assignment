a3f7328d15cb909124b72f2ac0eefe4e21aef4fd
Sachin <sachinkshuklaoo7@gmail.com>
2023-07-10 11:34:20 +0530

    style fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
